954989b3a6de3797c45264446186cfc23159d424
kalebeh2o <kalebef31@gmail.com>
2023-06-07 21:05:39 -0300

    ligação com o banco

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, Response, request
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import json
 
 app = Flask(__name__)
 
 from app.controllers import default
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/doacoes'
+#lembrar de passar tudo isso pra arquitetura MVC assim que possivel
+db = SQLAlchemy(app)
 
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    telefone = db.Column(db.String(13))
+
+    
 
